e225b63e66054f4f1ffa1bb19dca11544b918514
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2024-10-12 17:29:35 +0300

    trying to fix the deployment issues

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,22 @@
-// import cloudflare from '@sveltejs/adapter-cloudflare';
+import cloudflare from '@sveltejs/adapter-cloudflare';
 
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	kit: {
-// 		adapter: cloudflare()
-// 	}
-// };
-
-// export default config;
-
-
-import adapter from '@sveltejs/adapter-cloudflare';
-
-export default {
-  kit: {
-    adapter: adapter(),
-    env: {
-      dir: process.cwd()
-    }
-  }
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: cloudflare()
+	}
 };
+
+export default config;
+
+
+// import adapter from '@sveltejs/adapter-cloudflare';
+
+// export default {
+//   kit: {
+//     adapter: adapter(),
+//     env: {
+//       dir: process.cwd()
+//     }
+//   }
+// };
