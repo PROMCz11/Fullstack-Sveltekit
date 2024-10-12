c3ca863d0b68d791f8ad0158617e809b1abe7072
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2024-10-12 16:52:59 +0300

    Changing the env var set up

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import cloudflare from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-cloudflare';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: cloudflare()
-	}
+export default {
+  kit: {
+    adapter: adapter(),
+    env: {
+      dir: process.cwd()
+    }
+  }
 };
-
-export default config;
